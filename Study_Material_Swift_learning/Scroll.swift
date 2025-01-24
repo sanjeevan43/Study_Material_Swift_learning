eb7fd429541ba87ebdbc4010410321cc961006c1
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-24 16:44:11 +0530

    new

---
+++ Study_Material_Swift_learning/Scroll.swift
@@ -0,0 +1,58 @@
+//
+//  Scroll.swift
+//  Study_Material_Swift_learning
+//
+//  Created by sanjeevan on 24/01/25.
+//
+
+import SwiftUI
+
+struct Scroll: View {
+    var body: some View {
+        Text("sanjeevan")
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<10) { _ in
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("fedhar")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("blode bard")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)         }
+    }
+
+}
+
+#Preview {
+    Scroll()
+}
