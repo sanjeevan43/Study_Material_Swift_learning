0585722395334c22040335a5e8be3c150170068f
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-24 16:43:28 +0530

    Initial Commit

---
+++ Study_Material_Swift_learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Study_Material_Swift_learning
+//
+//  Created by sanjeevan on 24/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Study_Material_Swift_learning/Study_Material_Swift_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_Material_Swift_learningApp.swift
+//  Study_Material_Swift_learning
+//
+//  Created by sanjeevan on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_Material_Swift_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
